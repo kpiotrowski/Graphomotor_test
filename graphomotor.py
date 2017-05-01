f02f444aede8f20e35e43d27b0cc67d1139c420d
Kamil Piotrowski <kamil.piotrowski.pol@gmail.com>
2017-05-01 14:02:41 +0200

    segmentacja figur part 1

--- graphomotor.py
+++ graphomotor.py
@@ -1,5 +1,6 @@
+from __future__ import print_function
 import numpy as np
-
+import pprint
 
 def read(filename):
     """
@@ -32,17 +33,19 @@ def read(filename):
         "max_force": int,
         "avg_force:" float,
         "max_speed": float,
-        "avg_speed": float,         
+        "avg_speed": float,
         "line_brakes": int,
         "avg_width": int,
         "avg_height": int,
         "pos_x": (min, max),
-        "pos"y": (min, max),
+        "pos_y": (min, max),
         "line_len": float,
         "image": Image in hsv,
         "figures": [                #TODO
             {
                 "type": string,     #TODO
+                "pos_x": [min, min],#TODO
+                "pos_y": [max, max],#TODO
                 "max_force": int,   #TODO
                 "min_force": int,   #TODO
                 "avg_force": float, #TODO
@@ -73,12 +76,12 @@ def read(filename):
             result['data'].append(tmp)
         name_tags = result['file_name'].split('_')
         result['patient_id'] = int(name_tags[1])
-        for i,v in enumerate(['operation_num', 'surgery', 'gender', 'arm', 'hemisphere', 'treatment']):
+        for i, v in enumerate(['operation_num', 'surgery', 'gender', 'arm', 'hemisphere', 'treatment']):
             result[v] = str(name_tags[2][i])
     return set_test_details(result)
 
 
-def set_test_details(gdata, speed_points = 50):
+def set_test_details(gdata, speed_points=50):
     max_x, max_y = 0, 0
     min_x, min_y = -1, -1
     max_f, avg_f = 0, [0, 0]
@@ -92,26 +95,31 @@ def set_test_details(gdata, speed_points = 50):
     gdata['min_force'] = -1
     gdata['max_speed'] = 0
     for idx, i in enumerate(gdata['data']):
-        if i['x'] > max_x: max_x = i['x']
-        if i['y'] > max_y: max_y = i['y']
-        if i['x'] < min_x or min_x == -1: min_x = i['x']
-        if i['y'] < min_y or min_y == -1: min_y = i['y']
-        if i['force'] > max_f: max_f = i['force']
+        if i['x'] > max_x:
+            max_x = i['x']
+        if i['y'] > max_y:
+            max_y = i['y']
+        if i['x'] < min_x or min_x == -1:
+            min_x = i['x']
+        if i['y'] < min_y or min_y == -1:
+            min_y = i['y']
+        if i['force'] > max_f:
+            max_f = i['force']
         if i['force'] > 0:
             if gdata['min_force'] == -1 or gdata['min_force'] > i['force']:
                 gdata['min_force'] = i['force']
             avg_f[0] += i['force']
             avg_f[1] += 1
-            if prev is not None and prev['force']>0:
-                len = np.sqrt(pow(prev['x'] - i['x'], 2) + pow(prev['y'] - i['y'], 2))
-                gdata['line_len'] += len
+            if prev is not None and prev['force'] > 0:
+                gdata['line_len'] += np.sqrt(pow(prev['x'] - i['x'], 2) + pow(prev['y'] - i['y'], 2))
             avg_w += i['width']
             avg_h += i['height']
 
-        if speed_t0 == 0: speed_t0 = i['time']
-        if idx>0 and idx % speed_points == 0:
+        if speed_t0 == 0:
+            speed_t0 = i['time']
+        if idx > 0 and idx % speed_points == 0:
             speed_t1 = i['time']
-            speed = (gdata['line_len'] - speed_l)/(speed_t1-speed_t0)
+            speed = (gdata['line_len'] - speed_l) / (speed_t1 - speed_t0)
             speed_l = gdata['line_len']
             speed_t0 = i['time']
             if speed > gdata['max_speed']:
@@ -122,51 +130,135 @@ def set_test_details(gdata, speed_points = 50):
         prev = i
         # LINE BRAKES
         if state is not None:
-            if state == 0 and i['force'] > 0: state = 1
+            if state == 0 and i['force'] > 0:
+                state = 1
             if state > 0 and i['force'] == 0:
                 state = 0
                 gdata['line_brakes'] += 1
-        if state is None: state = (i['force'] > 0)
+        if state is None:
+            state = (i['force'] > 0)
 
-    gdata['avg_speed'] = avg_speed[0]/avg_speed[1]
+    gdata['avg_speed'] = avg_speed[0] / avg_speed[1]
     gdata['max_force'] = max_f
     gdata['pos_x'] = (min_x, max_x)
     gdata['pos_y'] = (min_y, max_y)
-    gdata['avg_force'] = avg_f[0]/avg_f[1]
-    gdata['avg_width'] = avg_w/avg_f[1]
-    gdata['avg_height'] = avg_h/avg_f[1]
+    gdata['avg_force'] = avg_f[0] / avg_f[1]
+    gdata['avg_width'] = avg_w / avg_f[1]
+    gdata['avg_height'] = avg_h / avg_f[1]
     return gdata
 
-def create_image(gdata, scale=20):
+
+def find_figures_merge(figures, small_merge=40):
+    def merge(prev, next):
+        next['pos_x'] = [min(next['pos_x'][0], prev['pos_x'][0]),
+                        max(next['pos_x'][1], prev['pos_x'][1])]
+        next['pos_y'] = [min(next['pos_y'][0], prev['pos_y'][0]),
+                        max(next['pos_y'][1], prev['pos_y'][1])]
+        next['points'] += prev['points']
+        next['zeros_ahead'] = prev['zeros_ahead']
+
+    #MAŁE PRZERWY
+    small_break = 1
+    while small_break:
+        small_break = 0
+        for idx, fig in enumerate(figures):
+            if idx>0 and fig['zeros_ahead'] < small_merge:
+                merge(figures[idx-1], fig)
+                figures.pop(idx-1)
+                small_break = 1
+                break
+
+
+
+
+    #ZAWIERAJĄ POKRYWAJĄCE SIĘ OBSZARY
+    same_area = 1
+    while same_area:
+        same_area = 0
+        prev = None
+        for fig in figures:
+            if prev is not None:
+                #TODO CHECK if fig and prev contains the same area. If yes merge it, set same_area = 1.
+                pass
+            prev = fig
+
+    return figures
+
+
+def find_figures(gdata):
+    groups = []
+    group = {"x": [0 , 0], "y": [0, 0], "points": 0, "zeros_ahead": 0}
+    zeros = 0
+    x = [0, 0]
+    y = [0, 0]
+    non_zeros = 0
+    prev = None
+    for i in gdata['data']:
+        if i['force'] == 0:
+            zeros += 1
+        if i['force'] > 0:
+            non_zeros += 1
+            if i['x'] > x[1]: x[1] = i['x']
+            if i['y'] > y[1]: y[1] = i['y']
+            if x[0] == 0 or i['x'] < x[0]: x[0] = i['x']
+            if y[0] == 0 or i['y'] < y[0]: y[0] = i['y']
+        if prev is not None and prev['force'] > 0 and i['force'] == 0:
+            groups.append(
+                {"pos_x": x, "pos_y": y, "points": non_zeros, "zeros_ahead": zeros }
+            )
+            x = [0, 0]
+            y = [0, 0]
+            zeros = 1
+            non_zeros = 0
+            pass
+        prev=i
+    # TODO Find point groups which create figures
+    # TODO recognize figures based on group dimensions and sth else?
+    groups = find_figures_merge(groups)
+    pprint.pprint(groups)
+    print(len(groups))
+    gdata['figures'] = groups
+    return gdata
+
+
+def create_image(gdata, scale=30, show_speed=True, show_figure_box = False):
     """
     Tworzy obraz w przestrzeni hsv przedstawiający przegieb testu grafomotorycznego.
     Wsp scale oznacza jka bardzo obraz ma być przeskalowany w stosunku do rozmiaru danych wejściowych
     Siła nacisku reprezentowana jest jako barwa (H): zielony - zółty - czerwony : słaba - średnia - wysoka
     Prędkość rysowania reprezentowana jest jako (V): większa wartość to większa prędkość
-    Kąty rysowania reprezentowane jako wektory wychodzące                                                           #TODO
+    Kąty rysowania reprezentowane jako wektory wychodzące                                                         #TODO
     :param gdata: dict
     :param scale: int
+    :param show_speed: bool
     :return: dict
     """
     # TODO draw vectors
-    w,h = int((gdata['pos_x'][1])/scale)+2, int((gdata['pos_y'][1])/scale)+2
+    w, h = int((gdata['pos_x'][1]) / scale) + 2, int((gdata['pos_y'][1]) / scale) + 2
     data = np.zeros((w, h, 3), dtype=np.float)
     cur_speed = gdata['avg_speed']
     for i in gdata['data']:
         if i['force'] > 0:
-            X,Y = i['x']/scale, i['y']/scale
-            if int(X) != X: Xt = [int(X), int(X)+1]
-            else: Xt = [int(X)]
-            if int(Y) != Y: Yt = [int(Y), int(Y)+1]
-            else: Yt = [int(Y)]
-            for x in Xt:
-                for y in Yt:
-                    if 'speed' in i:
-                        cur_speed = i['speed']
-                    color = 0.350 - (0.350 * i['force']/gdata['max_force'])
-                    data[x,y] = [color, 1, cur_speed/gdata['max_speed']]
+            x, y = i['x'] / scale, i['y'] / scale
+            if int(x) != x: xt = [int(x), int(x) + 1]
+            else: xt = [int(x)]
+            if int(y) != y: yt = [int(y), int(y) + 1]
+            else: yt = [int(y)]
+            for x in xt:
+                for y in yt:
+                    if 'speed' in i: cur_speed = i['speed']
+                    color = 0.350 - (0.350 * i['force'] / gdata['max_force'])
+                    if show_speed: data[x, y] = [color, 1, cur_speed / gdata['max_speed']]
+                    else: data[x, y] = [color, 1, 1]
 
+    if show_figure_box == True:
+        for fig in gdata['figures']:
+            for y in fig['pos_y']:
+                for x in range(fig['pos_x'][0], fig['pos_x'][1]):
+                    data[int(x/scale),int(y/scale)] = [1,0,1]
+            for x in fig['pos_x']:
+                for y in range(fig['pos_y'][0], fig['pos_y'][1]):
+                    data[int(x/scale),int(y/scale)] = [1,0,1]
 
     gdata["image"] = data
     return gdata
-
--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from matplotlib.colors import hsv_to_rgb
 
 
 if __name__ == "__main__":
-    data = graphomotor.read("dane/33100000000 (M)/01_33100000000_10MLLP.mtb")
-    data = graphomotor.create_image(data)
+    data = graphomotor.read("dane/33100000000 (M)/02_33100000000_10MRLP.mtb")
+    data = graphomotor.find_figures(data)
+    data = graphomotor.create_image(data, show_speed=True, show_figure_box=True)
 
-    plt.imshow(hsv_to_rgb(data["image"]), interpolation='none')
+    plt.imshow(hsv_to_rgb(data["image"]))
 
     print("Maksymalny nacisk: "+str(data["max_force"]))
     print("Minimalny nacisk: "+str(data['min_force']))
