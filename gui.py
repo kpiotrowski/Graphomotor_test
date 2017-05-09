3dad4454b4123bcdaa5aeebb058765aedda3f16a
lewinms <lewin.michal@gmail.com>
2017-05-09 08:44:07 +0200

    better gui

--- gui.py
+++ gui.py
@@ -19,6 +19,7 @@ class MainGui(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         tk.Tk.wm_title(self, "Graphomotor")
+        self.data = None
         self.panesCount = 0
         self.panes = []
         self.path = "No file"
@@ -31,8 +32,10 @@ class MainGui(tk.Tk):
         pady = 10
         controller = ttk.Frame(self)
         controller.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        pathFrame = ttk.Labelframe(controller, text="File directory")
-        pathFrame.grid(row=1, column=1, padx=padx, pady=pady, sticky=tk.W)
+        leftSide = ttk.Frame(controller)
+        leftSide.grid(row=1, column=1)
+        pathFrame = ttk.Labelframe(leftSide, text="File directory")
+        pathFrame.grid(row=1, column=1, padx=padx, pady=pady)
         buttonChoose = ttk.Button(pathFrame, text="Load file", command=lambda: self.chooseFile())
         buttonChoose.grid(row=1, column=1, padx=padx, pady=pady)
         self.pathLabel = ttk.Label(pathFrame, text=self.path)
@@ -40,7 +43,7 @@ class MainGui(tk.Tk):
         container = ttk.Frame(self)
         container.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         self.graph = Graph(container)
-        optionFrame = ttk.Labelframe(controller, text="Options")
+        optionFrame = ttk.Labelframe(leftSide, text="Options")
         optionFrame.grid(row=2, column=1, padx=padx, pady=pady, sticky=tk.W)
         self.speedVar = tk.BooleanVar()
         showSpeed = MyCheckButton(optionFrame, text="Show speed", variable=self.speedVar, onvalue=True, offvalue=False, command=lambda: self.createImage())
@@ -50,17 +53,60 @@ class MainGui(tk.Tk):
         showBoxes.grid(row=1, column=2, padx=padx, pady=pady, sticky=tk.W)
         showSpeed.select()
         showBoxes.select()
+        rightSide = ttk.Frame(controller)
+        rightSide.grid(row=1, column=2)
+        infoFrame = ttk.Labelframe(rightSide, text="Info")
+        infoFrame.pack(padx=padx, pady=pady)
 
-        # infoFrame = ttk.Labelframe(controller, text="Info")
-        # infoFrame.grid(row=1, column=2, padx=padx, pady=pady)
-        #
-        # textWidth = 20
-        #
-        # info1 = ttk.Labelframe(infoFrame, text="Maksymalny nacisk")
-        # info1.grid(row=1, column=1, padx=padx/2.0, pady=pady/2.0, sticky=tk.W)
-        # self.infoText1 = tk.Text(info1, width=textWidth, height=info1.winfo_height())
-        # self.infoText1.pack()
+        textWidth = 20
 
+        info1 = ttk.Labelframe(infoFrame, text="Maksymalny nacisk")
+        info1.grid(row=1, column=1, padx=padx/2.0, pady=pady/2.0, sticky=tk.W)
+        self.infoText1 = tk.Label(info1, text="")
+        self.infoText1.pack()
+
+        info2 = ttk.Labelframe(infoFrame, text="Minimalny nacisk")
+        info2.grid(row=2, column=1, padx=padx / 2.0, pady=pady / 2.0, sticky=tk.W)
+        self.infoText2 = tk.Label(info2, text="")
+        self.infoText2.pack()
+
+        info3 = ttk.Labelframe(infoFrame, text="Średni nacisk")
+        info3.grid(row=3, column=1, padx=padx / 2.0, pady=pady / 2.0, sticky=tk.W)
+        self.infoText3 = tk.Label(info3, text="")
+        self.infoText3.pack()
+
+        info4 = ttk.Labelframe(infoFrame, text="Maksymalna predkosc")
+        info4.grid(row=1, column=2, padx=padx / 2.0, pady=pady / 2.0, sticky=tk.W)
+        self.infoText4 = tk.Label(info4, text="")
+        self.infoText4.pack()
+
+
+        info5 = ttk.Labelframe(infoFrame, text="Średnia predkosc")
+        info5.grid(row=2, column=2, padx=padx / 2.0, pady=pady / 2.0, sticky=tk.W)
+        self.infoText5 = tk.Label(info5, text="")
+        self.infoText5.pack()
+
+        info6 = ttk.Labelframe(infoFrame, text="Oderwania")
+        info6.grid(row=3, column=2, padx=padx / 2.0, pady=pady / 2.0, sticky=tk.W)
+        self.infoText6 = tk.Label(info6, text="")
+        self.infoText6.pack()
+
+
+        info7 = ttk.Labelframe(infoFrame, text="Droga rysowania")
+        info7.grid(row=1, column=3, padx=padx / 2.0, pady=pady / 2.0, sticky=tk.W)
+        self.infoText7 = tk.Label(info7, text="")
+        self.infoText7.pack()
+
+
+        info8 = ttk.Labelframe(infoFrame, text="Średnia szerokość kątowa")
+        info8.grid(row=2, column=3, padx=padx / 2.0, pady=pady / 2.0, sticky=tk.W)
+        self.infoText8 = tk.Label(info8, text="")
+        self.infoText8.pack()
+
+        info9 = ttk.Labelframe(infoFrame, text="Średnia wysokość kątowa")
+        info9.grid(row=3, column=3, padx=padx / 2.0, pady=pady / 2.0, sticky=tk.W)
+        self.infoText9 = tk.Label(info9, text="")
+        self.infoText9.pack()
 
     def addPane(self, pane):
         print(self.panesCount)
@@ -86,28 +132,37 @@ class MainGui(tk.Tk):
         self.path = filedialog.askopenfilename()
         if self.path == () or self.path == "":
             self.path = ""
+            self.data = None
             return
         self.pathLabel.config(text=self.path)
+        self.data = graphomotor.read(self.path)
+        self.data = graphomotor.find_figures(self.data)
         self.createImage()
 
     def createImage(self):
-        if(not os.isfile(self.path)):
+        if(self.data is None):
             return
-        data = graphomotor.read(self.path)
-        data = graphomotor.find_figures(data)
-        data = graphomotor.create_image(data, show_speed=self.speedVar.get(), show_figure_box=self.boxesVar.get())
+        self.data = graphomotor.create_image(self.data, show_speed=self.speedVar.get(), show_figure_box=self.boxesVar.get())
+        self.graph.changePlot(hsv_to_rgb(self.data["image"]))
 
-        self.graph.changePlot(hsv_to_rgb(data["image"]))
-
-        print("Maksymalny nacisk: "+str(data["max_force"]))
-        print("Minimalny nacisk: "+str(data['min_force']))
-        print("Średni nacisk: "+str(data["avg_force"]))
-        print("Maksymalna prędkość: "+str(data['max_speed']))
-        print("Średnia prędkość: "+str(data['avg_speed']))
-        print("Oderwania: " +str(data["line_breaks"]))
-        print("Droga rysowania: "+str(data["line_len"]))
-        print("Średnia szerokość kątowa: "+str(data["avg_width"]))
-        print("Średnia wysokość kątowa: "+str(data["avg_height"]))
+        self.infoText1.config(text=self.data["max_force"])
+        self.infoText2.config(text=self.data['min_force'])
+        self.infoText3.config(text=self.data["avg_force"])
+        self.infoText4.config(text=self.data['max_speed'])
+        self.infoText5.config(text=self.data['avg_speed'])
+        self.infoText6.config(text=self.data["line_breaks"])
+        self.infoText7.config(text=self.data["line_len"])
+        self.infoText8.config(text=self.data["avg_width"])
+        self.infoText9.config(text=self.data["avg_height"])
+        # print("Maksymalny nacisk: "+str(self.data["max_force"]))
+        # print("Minimalny nacisk: "+str(self.data['min_force']))
+        # print("Średni nacisk: "+str(self.data["avg_force"]))
+        # print("Maksymalna prędkość: "+str(self.data['max_speed']))
+        # print("Średnia prędkość: "+str(self.data['avg_speed']))
+        # print("Oderwania: " +str(self.data["line_breaks"]))
+        # print("Droga rysowania: "+str(self.data["line_len"]))
+        # print("Średnia szerokość kątowa: "+str(self.data["avg_width"]))
+        # print("Średnia wysokość kątowa: "+str(self.data["avg_height"]))
 
 
 class Graph(Figure):
